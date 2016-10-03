224986b374b4cb690b34539c6f3628e598873bf8
oduvan <a.lyabah@checkio.org>
2016-10-03 17:15:27 +0300

    start

---
+++ verification/tests.py
@@ -0,0 +1,26 @@
+"""
+TESTS is a dict with all you tests.
+Keys for this will be categories' names.
+Each test is dict with
+    "input" -- input data for user function
+    "answer" -- your right answer
+    "explanation" -- not necessary key, it's using for additional info in animation.
+"""
+
+TESTS = {
+    "Basics": [
+        {
+            "input": [
+              "theme=light; sessionToken=abc123", "theme"
+            ],
+            "answer": "light"
+        },
+        {
+            "input": [
+              "_ga=GA1.2.447610749.1465220820; _gat=1; ffo=true", "ffo"
+            ],
+            "answer": "true"
+        },
+        
+    ]
+}
